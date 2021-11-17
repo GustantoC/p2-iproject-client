f045d93d9424671cb51fa74f6cc2e56ba672e554
GustantoC <GustantoC@gmail.com>
2021-11-17 21:49:44 +0700

    Finish List

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -50,7 +50,7 @@ import swal from "sweetalert2";
 
 const Toast = swal.mixin({
   toast: true,
-  position: "top-end",
+  position: "top-start",
   showConfirmButton: false,
   timer: 3000,
   timerProgressBar: true,
